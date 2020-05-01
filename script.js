9e7489835f781351958983ea8410fe522ed47f60
Scottiep79 <sapullen79@yahoo.com>
2020-05-01 01:35:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/*Use the following array.
+
+var rockets = [
+{ name:'Sally', age:32 },
+{ name:'Bob', age:23 },
+{ name:'Jim', age:16 },
+{ name:'Scott Rhine', age:7 },
+{ name:'Angie Davison', age:4 },
+{ name:'James Munger', age:3 }
+];
+Write a program that will print this array and an array with only people of age 18 or higher. Use filter.*/
